cd6d543eca14c54d64f6c42ea77fca66ba5d12be
NghiaPT <111112535+nghiapt2098@users.noreply.github.com>
2026-04-21 10:28:03 +0700

    update form create

---
+++ MaintenanceRequestApp/Migrations/20260421030621_UpdateKPIModels.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaintenanceRequestApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateKPIModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MaintenanceNotes_Users_UserId",
+                table: "MaintenanceNotes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Urgency",
+                table: "RequestMaintenances",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "MaintenanceNotes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MaintenanceNotes_Users_UserId",
+                table: "MaintenanceNotes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MaintenanceNotes_Users_UserId",
+                table: "MaintenanceNotes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Urgency",
+                table: "RequestMaintenances",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "MaintenanceNotes",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MaintenanceNotes_Users_UserId",
+                table: "MaintenanceNotes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
